d2c16c703e0d0fc50c88497527cc8296db451ed6
Teerth Laptop <teerthp2004@gmail.com>
2022-10-14 01:04:02 -0500

    Arrow impl???

--- src/pqvqntort/glyphs/model/Arrow.java
+++ src/pqvqntort/glyphs/model/Arrow.java
@@ -1,10 +1,25 @@
 package pqvqntort.glyphs.model;
 
-public sealed class Arrow permits StraightArrow, CurvedArrow, CircleArrow {
+public sealed interface Arrow permits Align, Radial, Fill {
+
 }
 
-final class StraightArrow extends Arrow {}
+enum Align implements Arrow{
+    VERTICAL,
+    HORIZONTAL,
+    ASCEND,
+    DESCEND;
+}
+enum Radial implements Arrow{
+    UP, UP_RIGHT,
+    RIGHT, DOWN_RIGHT,
+    DOWN, DOWN_LEFT,
+    LEFT, UP_LEFT;
 
-final class CurvedArrow extends Arrow {}
-
-final class CircleArrow extends Arrow {}
+    /*Radial opposite() {
+        return Radial.values()[(this.ordinal() + 4) % 8];
+    }*/
+}
+enum Fill implements Arrow {
+    DOT, RING;
+}
